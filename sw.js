3e5481f897b6bf42c81f4fcd01c1ef5dba0b11d7
arasyelle-a11y <a.rasyelle@web.de>
2026-07-29 19:06:53 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,1 @@
-
-const CACHE = "wo-ist-was-v1";
-const ASSETS = ["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
-  self.skipWaiting();
-});
-self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
-self.addEventListener("fetch", event => {
-  if(event.request.method !== "GET") return;
-  event.respondWith(
-    fetch(event.request).then(response => {
-      const copy = response.clone();
-      caches.open(CACHE).then(cache => cache.put(event.request, copy));
-      return response;
-    }).catch(()=>caches.match(event.request))
-  );
-});
+const C="wo-ist-was-v2";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./styles.css","./app.js"]))));self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
